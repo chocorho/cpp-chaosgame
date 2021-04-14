bc5b31c2f733daf6360263b0e0def8e0c0af124f
ichbineinnerd <xximtilixx@gmail.com>
2021-04-14 22:28:30 +0200

    add comments to resource.h

--- resource.h
+++ resource.h
@@ -1,11 +1,17 @@
 #ifndef RESOURCE_H
 #define RESOURCE_H
 
+//all macros in this file up to and including WINDOW_HEIGHT can (and should) be tweaked.
+
 //adds a bunch of logging
 #define DEBUG
 
+//defines the window's size
 #define WINDOW_WIDTH 700
 #define WINDOW_HEIGHT 700
+
+
+
 
 
 #define COMPONENT_MAINMENU 101
